cf83bc4caa9bf698990e0719a1886541d491412b
Shaikh sajid <sajidshaikh10822@gmail.com>
2026-09-09 19:34:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const SUPABASE_URL = "https://wyavyrphoqmdaslsfbzq.supabase.co";
-
-const SUPABASE_KEY =
-  "sb_publishable_tD-2Ekpx7d-jNkeyiTPj-w_mikOo0Yx";
+const SUPABASE_URL = "https://dcwdzbejpwvskajukxrh.supabase.co";
+const SUPABASE_KEY = "sb_publishable_1IsM8N7-OEcBJyIH5HnsFw_cl6F5oPU";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
+);
 );
 
 let allProducts = [];
